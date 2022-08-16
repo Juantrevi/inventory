218331d3041a40f2435f0163d614e91377d81d68
Juan Treviranus <juantrevi70@gmail.com>
2022-08-16 18:22:42 -0300

    "Servicio de obtener categoria por id"

--- src/main/java/com/companySkyTec/inventory/inventory/controllers/CategoryRestController.java
+++ src/main/java/com/companySkyTec/inventory/inventory/controllers/CategoryRestController.java
@@ -5,6 +5,7 @@ import com.companySkyTec.inventory.inventory.services.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +16,20 @@ public class CategoryRestController {
     @Autowired
     private ICategoryService service;
 
+    //Get all the categories
     @GetMapping("/categories")
     public ResponseEntity<CategoryResponseRest> searchCategories(){
 
         ResponseEntity<CategoryResponseRest> response = service.search();
+
+        return response;
+    }
+
+    //Get categories by id
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id){
+
+        ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 
         return response;
     }
--- src/main/java/com/companySkyTec/inventory/inventory/services/ICategoryService.java
+++ src/main/java/com/companySkyTec/inventory/inventory/services/ICategoryService.java
@@ -8,4 +8,10 @@ public interface ICategoryService {
 
     public ResponseEntity<CategoryResponseRest> search();
 
+    public ResponseEntity<CategoryResponseRest> searchById(Long id);
+
+
+
+
+
 }
